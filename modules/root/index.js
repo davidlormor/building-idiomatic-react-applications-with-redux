423b4019d7bca0566971f0327ec6a9829917b92b
David Lormor <davidlormor@gmail.com>
2016-06-04 11:35:53 -0500

    Adding React Router to the Project

--- modules/root/index.js
+++ modules/root/index.js
@@ -1,10 +1,16 @@
+// Dependencies
 import React from 'react'
 import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+
+// Components
 import App from '../app'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App} />
+    </Router>
   </Provider>
 )
 
