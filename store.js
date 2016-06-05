533e4ed0ce3242090ac19508ee151958855eb455
David Lormor <davidlormor@gmail.com>
2016-06-05 10:32:54 -0500

    Applying Redux Middleware

--- store.js
+++ store.js
@@ -1,44 +1,21 @@
-import { createStore } from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
+import promise from 'redux-promise'
+import createLogger from 'redux-logger'
 import reducer from './modules/reducers'
 
-const logger = store => next => {
-  if (!console.group) return next
-
-  return action => {
-    console.group(action.type)
-    console.log('%c prev state', 'color: gray', store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = next(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
-}
-
-const promise = store => next => action => {
-  if (typeof action.then === 'function') return action.then(next)
-
-  return next(action)
-}
-
-const wrapDispatchwithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware => {
-    store.dispatch = middleware(store)(store.dispatch)
-  })
-}
-
 export const configureStore = () => {
-  const store = createStore(
-    reducer,
-    {},
-    window.devToolsExtension && window.devToolsExtension()
-  )
-
+  // Promise middlware
   const middlewares = [promise]
 
-  if (process.env.NODE_ENV !== 'production') middlewares.push(logger)
+  // Conditionally apply logging middlware when not in production
+  if (process.env.NODE_ENV !== 'production') middlewares.push(createLogger())
 
-  wrapDispatchwithMiddlewares(store, middlewares)
-
-  return store
+  // Create store
+  return createStore(
+    reducer,
+    compose(
+      applyMiddleware(...middlewares),
+      window.devToolsExtension && window.devToolsExtension()
+    )
+  )
 }
