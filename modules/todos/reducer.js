e26fae489c26009f0551bda0545380d0fd35189c
David Lormor <davidlormor@gmail.com>
2016-06-05 08:55:27 -0500

    Normalizing the State Shape

--- modules/todos/reducer.js
+++ modules/todos/reducer.js
@@ -1,26 +1,50 @@
+import { combineReducers } from 'redux'
 import todo from '../todo/reducer'
 
-export default (todos = [], action) => {
+export const byId = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [
-        ...todos,
-        todo(null, action)
-      ]
     case 'TOGGLE_TODO':
-      return todos.map((t) => todo(t, action))
+      return {
+        ...state,
+        [action.id]: todo(state[action.id], action)
+      }
     default:
-      return todos
+      return state
   }
 }
 
-export const getVisibleTodos = (todos, filter) => {
+// Not sure why we need this...
+// Can't we just map the byId object to an array directly (i.e. via lodash, etc.)?
+export const allIds = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.id]
+    default:
+      return state
+  }
+}
+
+const todos = combineReducers({
+  byId,
+  allIds
+})
+
+export default todos
+
+const getAllTodos = (state) =>
+  state.allIds.map((id) => state.byId[id])
+
+export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state)
   switch (filter) {
     case 'all':
-      return todos
+      return allTodos
     case 'completed':
-      return todos.filter((t) => t.completed)
+      return allTodos.filter((t) => t.completed)
     case 'active':
-      return todos.filter((t) => !t.completed)
+      return allTodos.filter((t) => !t.completed)
+    default:
+      throw new Error(`Unknown filter: ${filter}.`)
   }
 }
--- store.js
+++ store.js
@@ -1,14 +1,16 @@
-import { compose, createStore } from 'redux'
-import reducers from './modules/reducers'
+import { createStore } from 'redux'
+import reducer from './modules/reducers'
 import { loadState, saveState } from './local-storage'
 import throttle from 'lodash/throttle'
 
 export const configureStore = () => {
   const initialState = loadState()
 
-  const store = createStore(reducers, initialState, compose(
-    window.devToolsExtension ? window.devToolsExtension() : (f) => f
-  ))
+  const store = createStore(
+    reducer,
+    initialState,
+    window.devToolsExtension && window.devToolsExtension()
+  )
 
   store.subscribe(throttle(() => {
     console.log('saving...')
